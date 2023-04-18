6535cb75ccd913183480925d48927235d8a9bde3
Samuel-devops <s.zamb@t-online.de>
2023-04-18 20:19:31 +0200

    Add project files.

---
+++ Domain/Flight.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+
+namespace Domain
+{
+	public class Flight
+	{
+		public int RemainingNumberOfSeats { get; set; }
+
+		public Flight(int seatCapacity)
+		{
+			RemainingNumberOfSeats = seatCapacity;
+		}
+
+		public object? Book(string passangerEmail, int numberOfSeats)
+		{
+			if (numberOfSeats > RemainingNumberOfSeats)
+			{
+				return new OverbookingError();
+			}
+			RemainingNumberOfSeats -= numberOfSeats;
+			return null;
+		}
+	}
+}
---
+++ FlightTest/FlightSpecifications.cs
@@ -0,0 +1,27 @@
+namespace FlightTest
+{
+	public class FlightSpecifications
+	{
+		[Fact]
+		public void Booking_reduces_the_number_of_seats()
+		{
+			var flight = new Flight(seatCapacity: 3);
+
+			flight.Book("test@email.com", 1);
+			flight.RemainingNumberOfSeats.Should().Be(2);
+		}
+
+		[Fact]
+		public void Avoids_overbooking()
+		{
+			// Given
+			var flight = new Flight(3);
+
+			// When
+			var error = flight.Book("test@email.com", 4);
+
+			// Then
+			error.Should().BeOfType<OverbookingError>();
+		}
+	}
+}
