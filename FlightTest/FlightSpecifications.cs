14d1e41309053afe7a982c513f0e79bd8235cf8f
Samuel-devops <s.zamb@t-online.de>
2023-04-19 15:38:31 +0200

    Test for multiple Values

--- FlightTest/FlightSpecifications.cs
+++ FlightTest/FlightSpecifications.cs
@@ -12,6 +12,24 @@ namespace FlightTest
 		}
 
 		[Fact]
+		public void Booking_reduces_the_number_of_seats_2()
+		{
+			var flight = new Flight(seatCapacity: 6);
+
+			flight.Book("test@email.com", 3);
+			flight.RemainingNumberOfSeats.Should().Be(3);
+		}
+
+		[Fact]
+		public void Booking_reduces_the_number_of_seats_3()
+		{
+			var flight = new Flight(seatCapacity: 10);
+
+			flight.Book("test@email.com", 4);
+			flight.RemainingNumberOfSeats.Should().Be(6);
+		}
+
+		[Fact]
 		public void Avoids_overbooking()
 		{
 			// Given
