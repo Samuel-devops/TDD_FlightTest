d4bec5c97cb4fd6b2d64264df9923b8f8734344d
Samuel-devops <s.zamb@t-online.de>
2023-04-25 21:32:49 +0200

    FlightApplicationSpecifications

---
+++ Application.Test/FlightApplicationSpecifications.cs
@@ -0,0 +1,34 @@
+﻿namespace Application.Test
+{
+	public class FlightApplicationSpecifications
+	{
+		[Fact]
+		public void Books_flights()
+		{
+			var bookingService = new BookingService();
+			bookingService.Book(new BookDTO());
+			bookingService.FindBookings().Should().ContainEquivalentOf(
+				new BookingRm());
+		}
+	}
+
+	public class BookingService
+	{
+		public void Book(BookDTO bookDTO)
+		{
+		}
+
+		public IEnumerable<BookingRm> FindBookings()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	public class BookDTO
+	{
+	}
+
+	public class BookingRm
+	{
+	}
+}
