bf1966f75c1432b2fa0211c5f89b8e5147648b1f
Samuel-devops <s.zamb@t-online.de>
2023-04-25 21:52:43 +0200

    Application Layer

--- Application.Test/FlightApplicationSpecifications.cs
+++ Application.Test/FlightApplicationSpecifications.cs
@@ -6,9 +6,13 @@
 		public void Books_flights()
 		{
 			var bookingService = new BookingService();
-			bookingService.Book(new BookDTO());
+
+			bookingService.Book(new BookDTO(
+				flightId: Guid.NewGuid(), passangerEmail: "test@mail.com", numberOfSeats: 2
+				));
+
 			bookingService.FindBookings().Should().ContainEquivalentOf(
-				new BookingRm());
+				new BookingRm(passangerEmail: "test@mail.com", numberOfSeats: 2));
 		}
 	}
 
@@ -20,15 +24,36 @@
 
 		public IEnumerable<BookingRm> FindBookings()
 		{
-			throw new NotImplementedException();
+			return new[]
+			{
+				new BookingRm(passangerEmail: "test@mail.com", numberOfSeats: 2)
+			};
 		}
 	}
 
 	public class BookDTO
 	{
+		public Guid FlightId { get; set; }
+		public string PassangerEmail { get; set; }
+		public int NumberOfSeats { get; set; }
+
+		public BookDTO(Guid flightId, string passangerEmail, int numberOfSeats)
+		{
+			this.FlightId = flightId;
+			this.PassangerEmail = passangerEmail;
+			this.NumberOfSeats = numberOfSeats;
+		}
 	}
 
 	public class BookingRm
 	{
+		public string PassangerEmail { get; set; }
+		public int NumberOfSeats { get; set; }
+
+		public BookingRm(string passangerEmail, int numberOfSeats)
+		{
+			this.PassangerEmail = passangerEmail;
+			this.NumberOfSeats = numberOfSeats;
+		}
 	}
 }
